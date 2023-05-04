24372cb83e0cd15d340a6706e10cfa59ffae3d53
ZachDorm <zach.dorminey@gmail.com>
2023-05-04 10:46:21 -0400

    In-class copy

--- streamlit.py
+++ streamlit.py
@@ -1,3 +1,36 @@
 import streamlit as st
+import leafmap.foliumap as leafmap
+import xyzservices.providers as xyz
+import basal_and_bark
+from basal_and_bark import basal_and_bark as basal
 
-st.title("Hello World")
+
+st.title("This is a demo of a map that allows the user to add a basemap to a map. There is a dropdown list, and a box where you can enter a url pointing to xyz services.")
+st.expander("test")
+m= basal_and_bark.Map(center = [40,-100], zoom = 4, test = "test", scroll_wheel_zoom = True)
+m= leafmap.Map(center = [40,-100], zoom = 4, test = "test", scroll_wheel_zoom = True)
+
+col1, col2 = st.columns([4,1])
+
+with col2:
+
+    radio = st.radio('URL or dropdown', ('URL', 'dropdown'))
+
+    if radio == 'URL':
+        input = st.text_input('Please enter url for basemap', 'Esri.WorldImagery')
+        #basemap = eval(f"xyz.{input}")
+        #url = basemap.build_url()
+        m.add_tile_layer(url=input, name="map", attribution = ' ')
+
+    else:
+        base = st.selectbox('Please select a basemap', ('Topo', 'ShadeRelief', 'Gray'))
+
+        if base == "Topo":
+            m.add_basemap(basemap= "Esri.WorldTopoMap")
+        if base == "ShadeRelief":
+            m.add_basemap(basemap= "Esri.WorldShadedRelief")
+        if base == "Gray":
+            m.add_basemap(basemap= "Esri.WorldGrayCanvas")
+with col1:
+
+    m.to_streamlit(height=700)
